dadbeb0cef9ab2dff971ddc30c238aaf201d5841
Kevin Herrbach <herrbach.kevin@gmail.com>
2020-07-27 16:58:17 +0200

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.css';
+import Vue from 'vue'
+import vuetify from "./vue/plugins/vuetify";
+import App from "./vue/App";
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+new Vue({
+    vuetify,
+    render: h => h(App)
+}).$mount("#app")
---
+++ assets/js/vue/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app>
+        <v-app-bar flat color="white" fixed>
+            <v-spacer />
+            <v-spacer />
+            <v-btn text @click="$vuetify.goTo('#welcome', {duration: 400})">Acceuil</v-btn>
+            <v-btn text @click="$vuetify.goTo('#nos-vins', {duration: 400})">Nos vins</v-btn>
+            <v-spacer />
+            <v-spacer />
+            <v-icon color="primary" x-large>mdi-glass-wine</v-icon>
+            <v-spacer />
+            <v-spacer />
+            <v-btn text @click="$vuetify.goTo('#notre-histoire', {duration: 400})">Notre histoire</v-btn>
+            <v-btn text>Contact</v-btn>
+            <v-spacer />
+<!--            <v-dialog v-model="loginDialog" width="500">-->
+<!--                <template v-slot:activator="{on}">-->
+<!--                    <v-btn text icon v-on="on"><v-icon>mdi-account</v-icon></v-btn>-->
+<!--                </template>-->
+<!--                <v-card class="pa-5">-->
+<!--                    <v-container>-->
+<!--                        <login-form v-if="login"/>-->
+<!--                        <register-form v-else/>-->
+<!--                        <v-row justify="center" class="mt-8">-->
+<!--                            <v-btn text v-if="login" @click="login = !login" color="primary">Créer un compte</v-btn>-->
+<!--                            <v-btn text v-else @click="login = !login" color="primary">Se connecter</v-btn>-->
+<!--                        </v-row>-->
+<!--                    </v-container>-->
+<!--                </v-card>-->
+<!--            </v-dialog>-->
+        </v-app-bar>
+        <v-content>
+            <div>Coucou</div>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: "App.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
