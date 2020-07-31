d1ea962712bd4ae3888cc4d8b4aa6b7b14aee5ca
Kevin Herrbach <herrbach.kevin@gmail.com>
2020-08-01 01:11:42 +0200

    Adds login route  + fixtures for user

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login()
+    {
+        $user = $this->getUser();
+        return $this->json([
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles()
+        ]);
+    }
+}
