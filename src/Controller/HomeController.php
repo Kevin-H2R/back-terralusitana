b121264f065bcf23da107a46d515fee2bfe776ff
Kevin Herrbach <herrbach.kevin@gmail.com>
2020-10-25 19:27:33 +0100

    Empties session basket when success page is reached

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,10 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $session;
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @Route("/", name="home")
      */
@@ -23,6 +30,7 @@ class HomeController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function success() {
+        $this->session->set('purchase-basket', []);
         return $this->render('home/success.html.twig');
     }
 }
