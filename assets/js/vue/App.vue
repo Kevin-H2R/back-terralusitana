0a04ce2bc6eb88d6cdd3581b2867920e5593fc6b
Kevin Herrbach <herrbach.kevin@gmail.com>
2020-12-04 16:04:27 +0100

    adds footer

--- assets/js/vue/App.vue
+++ assets/js/vue/App.vue
@@ -1,6 +1,7 @@
 <template>
     <v-app>
         <router-view />
+        <footer style="background-color: grey; font-size: 12px" class="white--text pa-3">© 2020 TerraLusitana</footer>
     </v-app>
 </template>
 
