e7275a0dabd265bdbfc1447d4035352d6c70c1d9
Kevin Herrbach <herrbach.kevin@gmail.com>
2021-02-03 18:52:10 +0100

    Removes useless comments

--- assets/js/vue/components/SocialMediaLinks.vue
+++ assets/js/vue/components/SocialMediaLinks.vue
@@ -1,14 +1,4 @@
 <template>
-<!--    <div style="position: fixed; top: 50%; left: 2.5%">-->
-<!--        <v-btn-->
-<!--                color="pink"-->
-<!--                fab-->
-<!--                dark-->
-<!--                small-->
-<!--        >-->
-<!--            <v-icon>mdi-plus</v-icon>-->
-<!--        </v-btn>-->
-<!--    </div>-->
     <div class="social-media-links">
         <v-btn fab icon color="error" href="https://www.instagram.com/terralusitana/" target="_blank">
             <v-icon>mdi-instagram</v-icon>
