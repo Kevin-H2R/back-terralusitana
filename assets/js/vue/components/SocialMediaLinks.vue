1a4e0667ef95e2ba3fdaca2e665c10249cf45ecc
Kevin Herrbach <herrbach.kevin@gmail.com>
2021-02-03 18:50:42 +0100

    Adds insta and fb links

---
+++ assets/js/vue/components/SocialMediaLinks.vue
@@ -0,0 +1,30 @@
+<template>
+<!--    <div style="position: fixed; top: 50%; left: 2.5%">-->
+<!--        <v-btn-->
+<!--                color="pink"-->
+<!--                fab-->
+<!--                dark-->
+<!--                small-->
+<!--        >-->
+<!--            <v-icon>mdi-plus</v-icon>-->
+<!--        </v-btn>-->
+<!--    </div>-->
+    <div class="social-media-links">
+        <v-btn fab icon color="error" href="https://www.instagram.com/terralusitana/" target="_blank">
+            <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+        <v-btn fab icon color="primary" href="https://www.facebook.com/terralusitana/" target="_blank">
+            <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "social-media-links"
+    }
+</script>
+
+<style scoped>
+
+</style>
